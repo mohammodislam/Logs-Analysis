77962f70666006ef47e690a400d423a8d77bbd9b
vagrant <vagrant@vagrant.vm>
2019-05-02 13:55:42 +0000

    optimized

--- number3.py
+++ number3.py
@@ -1,29 +1,33 @@
 import psycopg2
 
-
 db = psycopg2.connect("dbname=news")
-log_cursor_total = db.cursor()
-log_cursor_error = db.cursor()
+log_cursor = db.cursor()
 q_log = """
-SELECT time::timestamp::date, COUNT(time::timestamp::date) FROM log GROUP BY (time::timestamp::date)
+With ERRORS AS
+(SELECT COUNT(time::timestamp::date), time::timestamp::date
+FROM log 
+WHERE status != '200 OK' 
+GROUP BY (time::timestamp::date) 
+ORDER BY (time::timestamp::date) DESC),
+ALL_REQUEST AS
+(SELECT COUNT(time::timestamp::date), time::timestamp::date
+FROM log
+GROUP BY (time::timestamp::date) 
+ORDER BY (time::timestamp::date) DESC)
+
+Select * 
+From ERRORS 
+INNER JOIN ALL_REQUEST
+ON ERRORS.time::timestamp::date = ALL_REQUEST.time::timestamp::date;
 """
-q_log_err = """
-SELECT COUNT(time::timestamp::date) FROM log WHERE status != '200 OK' GROUP BY (time::timestamp::date) 
-"""
-log_cursor_total.execute(q_log)
-log_cursor_error.execute(q_log_err)
-logs = log_cursor_total.fetchall()
-log_errors = log_cursor_error.fetchall()
+log_cursor.execute(q_log)
+logs = log_cursor.fetchall()
 
-# for log in log_errors:
-#     print("Error: ",log)
-
-# for log in logs:
-#     print("Total: ", log)
-
-for err, log in zip(log_errors, logs):
-    answer = str(round(((err[0]/log[1])*100), 2))
-    if float(answer) > 1:
-        print(log[0], answer,"%")
-
-
+for log in logs:
+    errPercentage = round((log[0]/log[2])*100, 2)
+    timeMonthName = log[1].strftime("%B")
+    timeDay = log[1].strftime("%d")
+    timeYear = log[1].strftime("%Y")
+    if errPercentage > 1:
+        print(timeMonthName,timeDay,",",timeYear, "-",errPercentage, "% errors")
+    
