d1dd7ac02c1f1122bd3f2193f5763bec7ec52c4f
vagrant <vagrant@vagrant.vm>
2019-05-23 16:43:22 +0000

    code formatted & optimized

--- number3.py
+++ number3.py
@@ -1,33 +1,40 @@
+#!/usr/bin/env python3
 import psycopg2
+import sys
 
-db = psycopg2.connect("dbname=news")
+try:
+    db = psycopg2.connect("dbname=news")
+except psycopg2.Error as e:
+    print("Unable to connect!")
+    print(e.pgerror)
+    print(e.diag.message_detail)
+    sys.exit(1)
+else:
+    print("Connected!")
+
 log_cursor = db.cursor()
 q_log = """
 With ERRORS AS
 (SELECT COUNT(time::timestamp::date), time::timestamp::date
-FROM log 
-WHERE status != '200 OK' 
-GROUP BY (time::timestamp::date) 
+FROM log
+WHERE status != '200 OK'
+GROUP BY (time::timestamp::date)
 ORDER BY (time::timestamp::date) DESC),
 ALL_REQUEST AS
 (SELECT COUNT(time::timestamp::date), time::timestamp::date
 FROM log
-GROUP BY (time::timestamp::date) 
+GROUP BY (time::timestamp::date)
 ORDER BY (time::timestamp::date) DESC)
-
-Select * 
-From ERRORS 
+Select (ERRORS.count::numeric / ALL_REQUEST.count)*100
+From ERRORS
 INNER JOIN ALL_REQUEST
-ON ERRORS.time::timestamp::date = ALL_REQUEST.time::timestamp::date;
+ON ERRORS.time::timestamp::date = ALL_REQUEST.time::timestamp::date
+WHERE (ERRORS.count::numeric / ALL_REQUEST.count)*100 > 1;
 """
 log_cursor.execute(q_log)
 logs = log_cursor.fetchall()
 
 for log in logs:
-    errPercentage = round((log[0]/log[2])*100, 2)
-    timeMonthName = log[1].strftime("%B")
-    timeDay = log[1].strftime("%d")
-    timeYear = log[1].strftime("%Y")
-    if errPercentage > 1:
-        print(timeMonthName,timeDay,",",timeYear, "-",errPercentage, "% errors")
-    
+    print('{error}%'.format(
+        error=round(log[0], 2))
+    )
