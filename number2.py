44995d2d3295fed6edcaa7a17ce347f670daa685
vagrant <vagrant@vagrant.vm>
2019-05-23 16:42:48 +0000

    code formatted

--- number2.py
+++ number2.py
@@ -1,12 +1,23 @@
+#!/usr/bin/env python3
 import psycopg2
+import sys
 
-db = psycopg2.connect("dbname=news")
+try:
+    db = psycopg2.connect("dbname=news")
+except psycopg2.Error as e:
+    print("Unable to connect!")
+    print(e.pgerror)
+    print(e.diag.message_detail)
+    sys.exit(1)
+else:
+    print("Connected!")
+
 article_author_cursor = db.cursor()
-q_article_author ="""
-SELECT authors.name, COUNT(articles.title)
+q_article_author = """
+SELECT authors.name,COUNT(articles.title)
 FROM articles
 INNER JOIN authors ON articles.author = authors.id
-INNER JOIN log ON CONCAT('/article/', articles.slug) = log.path 
+INNER JOIN log ON CONCAT('/article/', articles.slug) = log.path
 GROUP BY authors.name
 ORDER BY COUNT(articles.title) DESC;
 """
@@ -14,4 +25,5 @@ article_author_cursor.execute(q_article_author)
 article_author = article_author_cursor.fetchall()
 
 for aa in article_author:
-    print(aa[0], "-",aa[1],"views")
+    print('{article_name} - {article_view}views'.format(
+        article_name=aa[0], article_view=aa[1]))
